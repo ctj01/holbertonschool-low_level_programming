90c3490604b65982f399932e0a2c6207a97ca072
ctj01 <flexdancer507@gamil.com>
2020-06-11 15:30:03 -0500

    loop and conditions

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int n = rand();
+if (n == 0)
+{
+printf("0 is zero/n");
+}
+else if (n > 0)
+{
+printf("%d%s", n, " is positive/n");
+}
+else
+{
+printf("%d%s", n, " is negative");
+}
+return (0);
+}
