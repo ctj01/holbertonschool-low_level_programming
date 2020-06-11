2158e0bde98b1dbbc0a073ea8f7e650140cffc8e
ctj01 <flexdancer507@gamil.com>
2020-06-11 16:33:58 -0500

    loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ else if (n == 0)
 {
 printf("Last digit of %i is %i and is 0\n", n, d);
 }
-else if (n > 0 && n < 6)
+else if (n > 0 & n < 6)
 {
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, d);
 }
