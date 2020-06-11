35bf756616286481489c14470e651410432ff801
ctj01 <flexdancer507@gamil.com>
2020-06-10 23:29:38 -0500

    c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+* argc (Argument count) Length of the argument vector
+* argv (Argument vector)Array of character pointers
+* Return: Always 0 (Success)
+*/
 int main(int argc, char *argv[])
-{  char string[80];
-   strcpy (string, "Programming is like building a multilingual puzzle");
-   puts(string);
-   return (0);
+{
+char string[80];
+strcpy(string, "Programming is like building a multilingual puzzle");
+puts(string);
+return (0);
 }
