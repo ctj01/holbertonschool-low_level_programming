efd516ef9f08c223c1aed7a7457b707e63b15fb3
ctj01 <flexdancer507@gamil.com>
2020-06-11 20:48:23 -0500

    loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 'a'; a <= 'z'; i++)
+{
+putchar(a);
+}
+for (i = 'A'; a <= 'Z'; i++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
