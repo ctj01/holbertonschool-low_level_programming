dc517105a30ae813cb973bb8081fa69508afa9a2
ctj01 <flexdancer507@gamil.com>
2020-06-11 20:23:12 -0500

    loops

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 int d = n % 10;
 if (d > 5)
 {
-printf("Last digit of %i is %i and is greater than 5\n", n, d);
+printf("Last digit of %d is %d and is greater than 5\n", n, d);
 }
 else if (d > 0 && d < 6)
 {
-printf("Last digit of %i is %i and is less than 6 and not 0\n", n, d);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 }
-else
+else if (d == 0)
 {
-printf("Last digit of %i is %i and is 0\n", n, d);
+printf("Last digit of %d is %d and is 0\n", n, d);
 }
 return (0);
 }
