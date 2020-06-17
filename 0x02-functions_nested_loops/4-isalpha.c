70c97d4ae7955688379db263a1da7c3b4a5d466f
ctj01 <flexdancer507@gamil.com>
2020-06-16 21:05:40 -0500

    functions

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,30 +1,22 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "holberton.h"
 /**
- *_isalpha - print 1 if is lower or uppercase and 0 if c is other wise
+ *_islower - print 1 if is lower and 0 if c is other wise
  *@c: is a parameter
  * Return: nothing
  */
+
 int _isalpha(int c)
 {
-if (isalpha(c))
-printf("1");
-else
-printf("0");
-}
-/**
- *main - entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+if (c >= 'a' && c <= 'z')
 {
-int r;
-r = _isalpha('H');
-r = _isalpha('o');
-r = _isalpha(108);
-r = _isalpha(';');
-putchar('\n');
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
 return (0);
+}
 }
