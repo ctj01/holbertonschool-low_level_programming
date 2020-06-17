0c9227d38dafa17e9c263838c4823e54bcd812e8
ctj01 <flexdancer507@gamil.com>
2020-06-17 13:13:23 -0500

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+i = 98;
+void positive_or_negative(int i);
+return (0);
+}
