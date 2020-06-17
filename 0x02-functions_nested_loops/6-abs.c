47f7fd3207bdf2630270becb09bd7a5c44cc0d4c
ctj01 <flexdancer507@gamil.com>
2020-06-16 21:46:57 -0500

    functions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_abs - return values
+ *@d: is a parameter
+ * Return: nothing
+ */
+
+int _abs(int d)
+{
+if (d < 0)
+{
+d = d * -1;
+return (d);
+}
+else
+{
+return (d);
+}
+}
