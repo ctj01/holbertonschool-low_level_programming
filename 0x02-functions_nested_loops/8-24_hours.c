c4e31174143859b8f59c00db472685ddfac72dba
ctj01 <flexdancer507@gamil.com>
2020-06-17 11:43:07 -0500

    functions

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,15 +10,14 @@ void jack_bauer(void)
 int m;
 for (h = 0; h < 24; h++)
 {
-_putchar(h / 10 + '0');
+for (m = 0; m < 60; m++)
+{_putchar(h / 10 + '0');
 _putchar(h % 10 + '0');
 _putchar(':');
-for (m = 0; m < 60; m++)
-{
 _putchar(m / 10 + '0');
 _putchar(m % 10 + '0');
 _putchar('\n');
 }
-m = 0; 
+m = 0;
 }
 }
