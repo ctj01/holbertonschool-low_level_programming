dfe17393bc9b671d28ab1affa9aaeec1ea378a57
ctj01 <flexdancer507@gamil.com>
2020-06-16 21:55:27 -0500

    functions

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,12 @@
 
 int print_last_digit(int d)
 {
+int num = 0;  
 if (d < 0)
 {
 d = d * -1;
 }
-d = d % 10;
-_putchar(d + '0');
-return (d);
+num = d % 10;
+_putchar(num + '0');
+return (num);
 }
