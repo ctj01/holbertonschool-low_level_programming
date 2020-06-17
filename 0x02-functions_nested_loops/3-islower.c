f29765e42336960ffc8e76c373a673bdbe050c42
ctj01 <flexdancer507@gamil.com>
2020-06-16 21:00:07 -0500

    functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,29 +1,18 @@
-#include <stdio.h>
-#include <ctype.h>
 #include "holberton.h"
 /**
  *_islower - print 1 if is lower and 0 if c is other wise
  *@c: is a parameter
  * Return: nothing
  */
+
 int _islower(int c)
 {
-if (islower(c))
-printf("1");
-else
-printf("0");
-}
-/**
- *main - entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+if (c >= 'a' && c <= 'z')
 {
-int r;
-r = _islower('H');
-r = _islower('o');
-r = _islower(108);
-putchar('\n');
+return (1);
+}
+else
+{
 return (0);
+}
 }
