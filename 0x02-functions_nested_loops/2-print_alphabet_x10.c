121f742674c2fa6e6ef127f45f5f718bc1fe55a5
ctj01 <flexdancer507@gamil.com>
2020-06-16 20:23:06 -0500

    functions

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,9 @@
-#include <stdio.h>
 #include "holberton.h"
 /**
  *print_alphabet_x10 - uses the print_alphabet func
  *
  *Return: nothing
  */
-
 void print_alphabet_x10(void)
 {int i;
 int r = 0;
@@ -13,19 +11,9 @@ while (r < 11)
 {
 for (i = 'a'; i <= 'z'; i++)
 {
-putchar(i);
+_putchar(i);
 }
 r++;
-putchar('\n'); 
+_putchar('\n');
 }
-}
-/**
- *main - entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-print_alphabet_x10();
-return (0);
 }
