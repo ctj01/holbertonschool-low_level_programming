0eb92b1c14421fc9cabe944868c4b9882272c65b
ctj01 <flexdancer507@gamil.com>
2020-06-17 20:59:20 -0500

    debugging

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * add - Entry point
+ * @a: Integer variable
+ * @b: Integer variable
+ * Return: opeartion between a plus b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
