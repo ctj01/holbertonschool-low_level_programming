9fd85a62a92bd7b2e3aefd9756d4fb39beb2476f
ctj01 <flexdancer507@gamil.com>
2020-06-18 22:14:24 -0500

    functions

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * print_diagonal - check the code for Holberton School students.
+ *@n: is a parameter
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int i = 0;
+	int e = 0;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			while (e < size)
+			{
+			_putchar('#');
+			e++;
+			}
+			_putchar('\n');
+			e = 0;
+		}
+
+	} else
+	{
+		_putchar('\n');
+	}
+}
