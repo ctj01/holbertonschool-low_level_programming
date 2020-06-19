0ea30ff85b2ed70351d0a0ad765c4224bd996403
ctj01 <flexdancer507@gamil.com>
2020-06-18 22:44:43 -0500

    functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("Fizz");
+			printf(" ");
+		} else if (i % 5 == 0)
+		{
+			printf("Buzz");
+			printf(" ");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		} else
+		{
+			printf("%d", i);
+			printf(" ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
