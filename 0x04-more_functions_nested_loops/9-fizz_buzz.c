0f48aeba411db8b197537bb39a7f125f92c7f38f
ctj01 <flexdancer507@gamil.com>
2020-06-18 23:08:36 -0500

    functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,24 +12,22 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (((i % 3) == 0) && ((i % 5) != 0))
 		{
 			printf("Fizz");
 			printf(" ");
 		}
-		if (i % 5 == 0)
+		else if (((i % 5) == 0) && ((i % 3) != 0))
 		{
 			printf("Buzz");
 			printf(" ");
 		}
-		if (i % 3 == 0 && i % 5 == 0)
+		else if (((i % 5) == 0) && ((i % 3) == 0))
 		{
 			printf("FizzBuzz");
 			printf(" ");
-		} else if (i == 100)
-		{
-			printf(" ");
-		} else
+		}
+		else
 		{
 			printf("%d", i);
 			printf(" ");
