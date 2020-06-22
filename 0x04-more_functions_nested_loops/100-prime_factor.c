aa808734ff1de38b8fbff9e115f0c7935661f481
ctj01 <flexdancer507@gamil.com>
2020-06-21 23:41:10 -0500

    functions

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ *main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int i;
+	long int N = 612852475143;
+	long int longnumber;
+	long int aux;
+
+for (i = 2; i < N; i += 3)
+{
+if (i % 2 != 0)
+{
+if (N % i == 0)
+{
+if (i > longnumber && longnumber != 0)
+{
+printf("%i\n", i);
+break;
+}
+longnumber = i;
+}
+}
+}
+	return (0);
+}
