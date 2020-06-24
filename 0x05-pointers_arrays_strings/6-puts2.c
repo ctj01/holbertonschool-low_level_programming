0a9f701c0e2d76f779962adbd1877926836dcc27
ctj01 <flexdancer507@gamil.com>
2020-06-23 19:07:00 -0500

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ *puts2 - check the code for Holberton School students.
+ *@str: is a parameter
+ * Return: nothing.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(i + '0');
+		i += 2;
+	}
+	_putchar('\n');
+
+
+}
