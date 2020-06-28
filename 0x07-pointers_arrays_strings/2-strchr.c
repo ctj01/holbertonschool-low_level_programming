dd4af94dcb4dc45290d41b557a4a7abe325b75cd
ctj01 <flexdancer507@gamil.com>
2020-06-28 16:20:31 -0500

    pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *_strchr - check the code for Holberton School students.
+ *@s : is a parameter
+ *@c : is a parameter
+ * Return: nothing.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	int status = 0;
+
+	for (i = 0; s[i] ; i++)
+	{
+
+		if (s[i] == c)
+		{
+			status = 1;
+			return (s + i);
+		}
+
+	}
+
+	if (status)
+	{
+		return (NULL);
+	}
+
+}
