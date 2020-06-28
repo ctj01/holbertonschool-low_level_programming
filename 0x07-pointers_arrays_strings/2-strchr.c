3c4a5e66cbd46726caf5977659932000ee425679
ctj01 <flexdancer507@gamil.com>
2020-06-28 16:49:11 -0500

    pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,21 +9,19 @@
 char *_strchr(char *s, char c)
 {
 	int i = 0;
-	int status = 0;
 
 	while (s[i] != '\0')
 	{
 
 		if (s[i] == c)
 		{
-			status = 1;
 			return (s + i);
 		}
 		i++;
 
 	}
 
-	if (status == 1)
+	if (s[i] == c)
 
 		return (s + i);
 
