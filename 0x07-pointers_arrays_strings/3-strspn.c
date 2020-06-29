fc1484f05516d4957c335026cf9eb5d9cdafade2
ctj01 <flexdancer507@gamil.com>
2020-06-28 20:36:52 -0500

    pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * _strspn - check the code for Holberton School students.
+ *@s : is a parameter
+ *@accept : is a parameter
+ * Return: nothing.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int d = 0;
+	int aux = 0;
+	unsigned int x = 0;
+
+	while (accept[i])
+	{
+		i++;
+	}
+	aux = i;
+	i = 0;
+	for (i; i <= aux; i++)
+	{
+		for (d = 0; d <= aux; d++)
+		{
+			if (accept[i] == s[d])
+			{
+				x = x + 1;
+			}
+
+		}
+	}
+
+	return (x);
+}
