dcd825d4d432fcd09cd906fd9d2d937717f2d64c
ctj01 <flexdancer507@gamil.com>
2020-06-29 23:10:43 -0500

    pointers

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *set_string - check the code for Holberton School students.
+ *@s : is a parameter
+ *@to : is a parameter
+ * Return: nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
