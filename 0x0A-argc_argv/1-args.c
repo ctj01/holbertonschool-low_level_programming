68cecc05c5430b7412a13c6b33f95acddfc1ebc9
ctj01 <flexdancer507@gamil.com>
2020-07-04 20:22:33 -0500

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - check the code for Holberton School students.
+ *@argc : is a parameter
+ *@argv : is a pparameter
+ * Return: nothing.
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	int i;
+
+	for (i = 0; i < argc - 1; i++)
+	{
+	}
+	printf("%i\n", i);
+
+	return (0);
+
+}
