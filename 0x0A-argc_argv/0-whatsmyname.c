a29f69504715304ac0fdf57afa0ec4c664c43ba5
ctj01 <flexdancer507@gamil.com>
2020-07-04 19:37:38 -0500

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - check the code for Holberton School students.
+ *@i : is a parameter
+ *@argv : is a parameter
+ * Return: nothing.
+ */
+
+int main(int i, char *argv[])
+{
+	i = 0;
+
+	printf("%s\n", argv[i]);
+	return (0);
+}
