38230bd85554606e77994186485aca440a552438
ctj01 <flexdancer507@gamil.com>
2020-07-06 17:05:22 -0500

    arguments

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+ *main - check the code for Holberton School students.
+ *@argc : is a parameter
+ *@argv : is a pparameter
+ * Return: nothing.
+ */
+
+int main(int argc, char *argv[])
+{
+	int aux = 0, d = 0, i = 0;
+	int money[5] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("error\n");
+		return (0);
+	}
+	d = atoi(argv[1]);
+	if (d < 0)
+		printf("0\n");
+	for (i = 0; i < 5; ++i)
+	{
+		if (d / money[i] > 0)
+		{
+			aux += d / money[i];
+			if ((d % money[i]) == 0)
+			{
+				printf("%i\n", aux);
+				return (aux);
+			}
+			else
+				d = d % money[i];
+		}
+	}
+	return (0);
+}
