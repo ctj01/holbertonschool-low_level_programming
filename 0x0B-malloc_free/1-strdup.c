66c325bc0e11bfbf2cbc837f0373a556c98e2ac6
ctj01 <flexdancer507@gamil.com>
2020-07-07 16:46:56 -0500

    malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *_strlen_recursion - check the code for Holberton School students.
+ *@s : is a parameter
+ * Return: nothing.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int z = 0;
+
+	if (*s != '\0')
+	{
+		z =  _strlen_recursion(++s);
+		z++;
+	}
+	return (z);
+
+}
+
+/**
+ *_strdup - check the code for Holberton School students.
+ *@str : is a parameter
+ * Return: nothing.
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int d = 0;
+	char *dup = NULL;
+
+	i = _strlen_recursion(str);
+	dup = (char *)malloc(i);
+	if (dup)
+	{
+		for (d = 0; d < i; d++)
+		{
+			dup[d] = str[d];
+		}
+	}
+
+	return (dup);
+}
