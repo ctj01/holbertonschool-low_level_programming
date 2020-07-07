1f5affa048bcbe324d815b5cd253c5e5a985f999
ctj01 <flexdancer507@gamil.com>
2020-07-07 15:51:30 -0500

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,13 +14,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	arr = (char *)malloc(size);
-	if (size != 0 || arr != NULL)
+	if (size != 0 && arr != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
 			arr[i] = c;
 		}
-		return (arr);
 	}
 	return (arr);
 
