fd52d3817d6079e01c09fb706b8dcf89b90afff5
ctj01 <flexdancer507@gamil.com>
2020-07-07 17:27:26 -0500

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ int _strlen_recursion(char *s)
 {
 	int z = 0;
 
-	if (*s != 00)
+	if (*s != '\0')
 	{
 		z =  _strlen_recursion(++s);
 		z++;
@@ -37,7 +37,7 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 	i = _strlen_recursion(str);
-	dup = (char *)malloc((i + 1));
+	dup = (char *)malloc(i * sizeof(char)+ 1);
 	for (d = 0; d < i; d++)
 	{
 		dup[d] = str[d];
