452dd952bcf731ec354e52431f4425671f81fb00
ctj01 <flexdancer507@gamil.com>
2020-07-07 17:28:31 -0500

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -37,7 +37,7 @@ char *_strdup(char *str)
 		return (NULL);
 	}
 	i = _strlen_recursion(str);
-	dup = (char *)malloc(i * sizeof(char)+ 1);
+	dup = (char *)malloc(i + 1);
 	for (d = 0; d < i; d++)
 	{
 		dup[d] = str[d];
