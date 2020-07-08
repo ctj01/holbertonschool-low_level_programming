46041a99a9c39f24ef1e32c708cf545d82292478
ctj01 <flexdancer507@gamil.com>
2020-07-07 21:01:57 -0500

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,15 +13,21 @@ int **alloc_grid(int width, int height)
 	int i = 0, d = 0;
 	int **matriz = NULL;
 
+	if (height <= 0 || width <= 0)
+		return (matriz);
+
 	matriz = (int **)malloc(height * sizeof(int));
-	for (i = 0; i < height; i++)
+	if (matriz != NULL)
 	{
-		matriz[i] = (int *)malloc(width * sizeof(int));
-	}
-	for (i = 0; i < height; i++)
-	{
-		for (d = 0; d < width; d++)
+		for (i = 0; i < height; i++)
 		{
+			matriz[i] = (int *)malloc(width * sizeof(int));
+		}
+		for (i = 0; i < height; i++)
+		{
+			for (d = 0; d < width; d++)
+			{
+			}
 		}
 	}
 	return (matriz);
