cb95e8d84dc58234da10106b6eb577ccd7ff015c
ctj01 <flexdancer507@gamil.com>
2020-07-07 22:19:36 -0500

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,7 +13,7 @@ int **alloc_grid(int width, int height)
 	int i = 0, d = 0;
 	int **matriz = NULL;
 
-	if (height <= 0 || width <= 0)
+	if (height <= 0 && width <= 0)
 		return (NULL);
 	matriz = (int **)malloc(height * sizeof(int));
 	if (matriz == NULL)
