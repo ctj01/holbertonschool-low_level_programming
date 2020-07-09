961a238ccd1328f2080bbac97b68b43d75f6766b
ctj01 <flexdancer507@gamil.com>
2020-07-09 15:22:23 -0500

    malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ *@s : is a parameter
+ * Return: nothing.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int z = 0;
+
+	if (*s != '\0')
+	{
+		z =  _strlen_recursion(++s);
+		z++;
+	}
+	return (z);
+
+}
+
+/**
+ * string_nconcat - check the code for Holberton School students.
+ *@s1 : is a parameter
+ *@s2 : is a parameter
+ *@n : is a parameter
+ * Return: nothing.
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int d = 0, d2 = 0, i = 0, z = 0;
+	char *pointer = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	d = _strlen_recursion(s1);
+	d2  = _strlen_recursion(s2);
+
+	if (n >= d2)
+	{
+		pointer = malloc(sizeof(*pointer) * (d + d2 + 1));
+	}
+	else
+	{
+		pointer = malloc(sizeof(*pointer) * (d + n + 1));
+	}
+
+	if (pointer)
+	{
+		for (i = 0; i < s1[i]; i++)
+			pointer[i] = s1[i];
+
+		for (z = 0; z < s2[z] && z < n; z++)
+			pointer[i++] = s2[z];
+	}
+	pointer[i] = '\0';
+	return (pointer);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -3,5 +3,7 @@
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
+int _strlen_recursion(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 
 #endif /* _HOLBERTON_H_ */
