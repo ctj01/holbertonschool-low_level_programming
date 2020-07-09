227ca68f0f1012517ced8aee0fde7e648d49ee42
ctj01 <flexdancer507@gamil.com>
2020-07-09 12:55:07 -0500

    malloc

---
+++ 0x0C-more_malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ *malloc_checked - check the code for Holberton School students.
+ *@b : is a parameter
+ * Return: nothing.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if( p == NULL)
+	{
+		exit (98);
+	}
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+**malloc , exit and string literal**
