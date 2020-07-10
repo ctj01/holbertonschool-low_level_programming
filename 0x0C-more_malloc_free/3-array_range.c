ba6ca7df491f486a9bb94797cd35562d43856f79
ctj01 <flexdancer507@gamil.com>
2020-07-09 21:57:59 -0500

    malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,21 +12,22 @@ int *array_range(int min, int max)
 {
 	int *pointer  = NULL;
 	int i = 0;
+	int d = 0;
 
 	if (min > max)
 	{
 		return (pointer);
 	}
-	pointer = malloc(sizeof(int) * (min + max));
+	d = max - min  + 1;
+	pointer = malloc(sizeof(int) * d);
 
 	if (pointer)
-
-		for (i = 0; i < (min + max); i++)
+	{
+		for (i = 0; i < d; i++)
 		{
 			pointer[i] = i;
 		}
-	else
-		free(pointer);
+	}
 
 	return (pointer);
 }
