f94b9e6e5b21e491922a3f086a225cd8c91815ad
ctj01 <flexdancer507@gamil.com>
2020-07-13 22:20:56 -0500

    structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,6 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strcopy(char *s);
+void free_dog(dog_t *d);
 
 
 
