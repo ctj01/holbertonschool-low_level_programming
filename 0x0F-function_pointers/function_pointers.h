c928657c1ab17ca9959617861947af21a5b3ea9f
ctj01 <flexdancer507@gamil.com>
2020-07-15 20:54:50 -0500

    function pointer

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef FUNCTION_POINTERS
-#define FUNCTION_POINTERS
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 #include <stdio.h>
 
 void print_name(char *name, void (*f)(char *));
