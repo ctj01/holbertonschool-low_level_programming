743bc36a72dc64fbcaa0dd251285f24dea6c2bde
ctj01 <flexdancer507@gamil.com>
2020-07-15 21:37:12 -0500

    function pointer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,8 +11,10 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	if (array && size > 0)
-		for (int i = 0; i < size; i++)
+	int i;
+
+	if (array && cmp)
+		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]))
 				return (i);
