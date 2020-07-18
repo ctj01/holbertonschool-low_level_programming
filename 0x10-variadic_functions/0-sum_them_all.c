6dc092e2ac929992b4c345556353917b23cf3a75
ctj01 <flexdancer507@gamil.com>
2020-07-18 13:43:56 -0500

    variadic_functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,13 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
-	int sum;
-
-	sum = 0;
+	int sum = 0;
+	va_list list;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list list;
 
 	va_start(list, n);
 	for (i = 0; i < n; i++)
