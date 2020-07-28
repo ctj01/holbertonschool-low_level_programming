70bf9552e9bd8f3ced80aa03ccac76f40a65ffc2
ctj01 <flexdancer507@gamil.com>
2020-07-28 08:31:38 -0500

    adding null option

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		{
 			newnodo->next = *head;
 			*head = newnodo;
-			return (newnodo);
+			return (*head);
 		}
 
 		aux = *head;
