c9c2f52f35be891e948213fa0532ebe353e0e5d0
ctj01 <flexdancer507@gamil.com>
2020-07-28 08:37:07 -0500

    fixing function

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -13,14 +13,12 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *newnodo = NULL;
-	listint_t *aux;
 
 	newnodo = malloc(sizeof(listint_t));
 	if (newnodo == NULL)
 		return (newnodo);
 	newnodo->n = n;
 	newnodo->next = NULL;
-	aux = *head;
 	if (*head == NULL)
 	{
 		*head = newnodo;
